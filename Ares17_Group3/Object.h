92c9ac28d944a1ed4d16a72c40f688f609c72533
Matteo <matteo.marcuzzo.it@gmail.com>
2016-11-02 21:59:13 +0000

    Object loader added

--- Ares17_Group3/Object.h
+++ Ares17_Group3/Object.h
@@ -1,10 +1,16 @@
 #ifndef OBJECT
 #define OBJECT
 #include "MeshManager.h"
+#include <vector>
+using namespace std;
 
 class Object {
 private:
-
+	// cube is used for the walls and the skybox
+	vector<GLfloat> verts; // contains vertices of loaded object
+	vector<GLfloat> norms; // contains normal of loaded object
+	vector<GLfloat> tex_coords; // contains texture coordinates of loaded object
+	vector<GLuint> indices; // contains indices of loaded object
 
 protected:
 
