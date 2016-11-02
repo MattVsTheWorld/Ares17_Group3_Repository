42d47e755175e1138d711e397a610402305481d6
Matteo <matteo.marcuzzo.it@gmail.com>
2016-11-02 22:55:28 +0000

    Jk I was stupid I fixed it

--- Ares17_Group3/SDLmanager.cpp
+++ Ares17_Group3/SDLmanager.cpp
@@ -12,22 +12,31 @@ namespace SDLmanager {
 		exit(1);
 	}
 
-	bool handleSDLEvent(SDL_Event const &sdlEvent)
-	{
-		if (sdlEvent.type == SDL_QUIT)
-			return false;
-		if (sdlEvent.type == SDL_KEYDOWN)
-		{
-			// Can extend this to handle a wider range of keys
-			switch (sdlEvent.key.keysym.sym)
-			{
-			case SDLK_ESCAPE:
-				return false;
-			default:
-				break;
-			}
-		}
-		return true;
+	SDL_Window * setupRC(SDL_GLContext &context) {
+		SDL_Window * window;
+		if (SDL_Init(SDL_INIT_VIDEO) < 0) // Initialize video
+			exitFatalError("Unable to initialize SDL");
+
+		// Request an OpenGL 3.0 context.
+
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+		SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);  // double buffering on
+		SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8); // 8 bit alpha buffering
+		SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
+		SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4); // Turn on x4 multisampling anti-aliasing (MSAA)
+
+														   // Create 800x600 window
+		window = SDL_CreateWindow("SDL/GLM/OpenGL Demo", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+			800, 600, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+		if (!window) // Check window was created OK
+			exitFatalError("Unable to create window");
+
+		context = SDL_GL_CreateContext(window); // Create opengl context and attach to window
+		SDL_GL_SetSwapInterval(1); // set swap buffers to sync with monitor's vertical refresh rate
+		return window;
 	}
 
 }
--- Ares17_Group3/main.cpp
+++ Ares17_Group3/main.cpp
@@ -9,38 +9,35 @@
 
 
 // Program entry point - SDL manages the actual WinMain entry point for us
-int main(int argc, char *argv[])
-{
-	SDL_GLContext glContext; // OpenGL context handle
+int main(int argc, char *argv[]) {
+
+
 	SDL_Window * hWindow; // window handle
+	SDL_GLContext glContext; // OpenGL context handle
+	hWindow = SDLmanager::setupRC(glContext); // Create window and render context 
 
-	if (SDL_Init(SDL_INIT_VIDEO) < 0) // Initialize video
-		SDLmanager::exitFatalError("Unable to initialize SDL");
-
-
-							  // Create 800x600 window
-	hWindow = SDL_CreateWindow("SDL Test",
-		100, 100, 800, 600,
-		SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
-
-	if (!hWindow) // Check window was created OK
-		SDLmanager::exitFatalError("Unable to create window");
-
-
-	glContext = SDL_GL_CreateContext(hWindow);
+								  // Required on Windows *only* init GLEW to access OpenGL beyond 1.1
+	glewExperimental = GL_TRUE;
+	GLenum err = glewInit();
+	if (GLEW_OK != err) { // glewInit failed, something is seriously wrong
+		std::cout << "glewInit failed, aborting." << endl;
+		exit(1);
+	}
+	cout << glGetString(GL_VERSION) << endl;
 	SceneManager::init();
 
-	SDL_Event sdlEvent;	// variable to detect SDL events
-	bool running = true;
-	while (running)		// the event loop
-	{
-		SDL_PollEvent(&sdlEvent);
-		running = SDLmanager::handleSDLEvent(sdlEvent);
-		SceneManager::update();		
-		SceneManager::draw(hWindow);		
+	bool running = true; // set running to true
+	SDL_Event sdlEvent;  // variable to detect SDL events
+	while (running) {	// the event loop
+		while (SDL_PollEvent(&sdlEvent)) {
+			if (sdlEvent.type == SDL_QUIT)
+				running = false;
+		}
+		SceneManager::update();	// update function
+		SceneManager::draw(hWindow); // draw function
 	}
 
-
+	SDL_GL_DeleteContext(glContext);
 	SDL_DestroyWindow(hWindow);
 	SDL_Quit();
 	return 0;
