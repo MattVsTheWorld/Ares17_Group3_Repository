ae244e4224f0bfd107cee501590a2f444ae04d23
Matteo <matteo.marcuzzo.it@gmail.com>
2016-11-17 17:53:08 +0000

    OPS

--- Ares17_Group3/SceneManager.cpp
+++ Ares17_Group3/SceneManager.cpp
@@ -143,7 +143,7 @@ namespace SceneManager {
 		glm::mat4 model;
 		model = glm::translate(model, glm::vec3(0.0f, -1.75f, 0.0f)); // Translate it down a bit so it's at the center of the scene
 		model = glm::scale(model, glm::vec3(0.2f, 0.2f, 0.2f));	// It's a bit too big for our scene, so scale it down
-		glUniformMatrix4fv(glGetUniformLocation(shader->Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+		glUniformMatrix4fv(glGetUniformLocation(shader->Program, "modelMatrix"), 1, GL_FALSE, glm::value_ptr(model));
 		ourModel->Draw(shader);
 		//glBindTexture(GL_TEXTURE_2D, testCube->object_getTexture());
 		
