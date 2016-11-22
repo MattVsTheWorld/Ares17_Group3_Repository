fd9b9f54b6d05d6395e03aed889f19625b2746a4
Matteo Marcuzzo <matteo.marcuzzo.it@gmail.com>
2016-11-22 16:05:46 +0000

    Pushing for backup; work on physics class

--- Ares17_Group3/Physics.cpp
+++ Ares17_Group3/Physics.cpp
@@ -7,14 +7,68 @@ Physics::Physics() {
 // EXTREMELY simplistic gravity; will modify it once I get home
 float Physics::applyGravity(glm::vec3 pos) {
 	if (pos.y > GROUND_POSITION) pos.y -= gravity;
-	return pos.y;
-}
-
-/*
-float Physics::applyGravity(glm::vec3 pos) {
-	if (pos.y > GROUND_POSITION) {
-		pos.y -= gravity;
+	currentTime = clock();
+	if (currentTime > lastTime + DT_MILLISECONDS) { // operations done every ~33ms
+		//	printf("Diff: %d\n", currentTime - lastTime);
+		
+		unsigned int dt = currentTime - lastTime;
+		float dt_secs = (float)dt / 1000;
+		std::cout << dt_secs << std::endl;
+		lastTime = currentTime;
+		//printf()
 	}
 	return pos.y;
 }
-*/
+
+// this needs to return a pair
+float Physics::applyGravity(glm::vec3 pos, glm::vec3 vel, glm::vec3 acc) {
+//	if (pos.y > GROUND_POSITION) pos.y -= gravity;
+
+	glm::vec3 vel2(0.0 ,0.0, 0.0);
+	glm::vec3 pos2(0.0, 0.0, 0.0);
+	glm::vec3 avg_vel(0.0, 0.0, 0.0);
+	currentTime = clock();
+	if (currentTime > lastTime + DT_MILLISECONDS) { // operations done every ~33ms
+		//	printf("Diff: %d\n", currentTime - lastTime);
+		
+		unsigned int dt = currentTime - lastTime;
+		float dt_secs = (float)dt / 1000;
+		std::cout << dt_secs << std::endl;
+		lastTime = currentTime;
+
+		// accelerations
+		// 		acc.y += ((G * mass[k]) / (pow(r, 2) + pow(epsilon, 2)) * sin(angle));  // y component
+		//		acc.x += ((G * mass[k]) / (pow(r, 2) + pow(epsilon, 2)) * cos(angle)); // x component
+		acc.y += 0;
+		acc.x += 0;
+		acc.z += 0;
+
+		// euler's method
+		vel2.y = vel.y + acc.y * dt_secs; // next velocity
+		avg_vel.y = (vel.y + vel2.y) / 2; // average velocoiy
+		pos2.y = pos.y + avg_vel.y * dt_secs; // new position
+										   // same for x component
+		vel2.x = vel.x + acc.x * dt_secs;
+		avg_vel.x = (vel.x + vel2.x) / 2;
+		pos2.x = pos.x + avg_vel.x * dt_secs;
+
+		vel2.z = vel.z + acc.z * dt_secs;
+		avg_vel.z = (vel.z + vel2.z) / 2;
+		pos2.z = pos.z + avg_vel.z * dt_secs;
+
+	//	bodies[i]->moveTo(pos2.x, pos2.y); // move to new position
+										   // update values
+		pos.y = pos2.y;
+		vel.y = vel2.y;
+
+		pos.x = pos2.x;
+		vel.x = vel2.x;
+
+		pos.z = pos2.z;
+		vel.z = vel2.z;
+		
+
+	}
+	return pos.y;
+	// return position and velocity :thinking:
+}
--- Ares17_Group3/Physics.h
+++ Ares17_Group3/Physics.h
@@ -6,21 +6,29 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+// debug
+#include <iostream>
+#include <time.h>
 
 using namespace std;
 #define GRAVITY_VALUE 0.005
 #define GROUND_POSITION 0.5
+#define DT_MILLISECONDS 20
+// this seems to give a time step of 0.033 seconds
+// Seems strange, will have to check if reliable
 
 
 class Physics {
 private:
 	const float gravity = GRAVITY_VALUE;
-	// 
+	// debug
+	unsigned int lastTime = 0, currentTime;
 protected:
 public:
 	Physics();
 	// simplistic definition	
 	float applyGravity(glm::vec3 pos);
+	float applyGravity(glm::vec3 pos, glm::vec3 vel, glm::vec3 acc);
 
 
 };
