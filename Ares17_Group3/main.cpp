136375c8bc23cc34f2ba975d630d222ab7221539
Matteo <matteo.marcuzzo.it@gmail.com>
2017-01-18 12:16:58 +0000

    This branch has basically become a work thing for bullet // only console display for now

--- Ares17_Group3/main.cpp
+++ Ares17_Group3/main.cpp
@@ -20,8 +20,7 @@ std::vector<btRigidBody*> bodies;
 
 // Program entry point - SDL manages the actual WinMain entry point for us
 int main(int argc, char *argv[]) {
-
-
+/*
 	SDL_Window * hWindow; // window handle
 	SDL_GLContext glContext; // OpenGL context handle
 	hWindow = SDLmanager::setupRC(glContext); // Create window and render context 
@@ -29,7 +28,7 @@ int main(int argc, char *argv[]) {
 											  // Required on Windows *only* init GLEW to access OpenGL beyond 1.1
 	glewExperimental = GL_TRUE;
 	GLenum err = glewInit();
-
+	*/
 	///+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	///+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	///+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -191,14 +190,14 @@ int main(int argc, char *argv[]) {
 	///+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	///+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	///+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
+	/*
 	if (GLEW_OK != err) { // glewInit failed, something is seriously wrong
 		std::cout << "glewInit failed, aborting." << endl;
 		exit(1);
 	}
 	cout << glGetString(GL_VERSION) << endl;
 //	SceneManager::init();
-
+*/
 	bool running = true; // set running to true
 	SDL_Event sdlEvent;  // variable to detect SDL events
 	while (running) {	// the event loop
@@ -211,8 +210,10 @@ int main(int argc, char *argv[]) {
 
 	}
 
-	SDL_GL_DeleteContext(glContext);
-	SDL_DestroyWindow(hWindow);
-	SDL_Quit();
+//	SDL_GL_DeleteContext(glContext);
+//	SDL_DestroyWindow(hWindow);
+//	SDL_Quit();
 	return 0;
+
+	
 }
